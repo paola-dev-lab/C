2d00898be1e2227aa8bbfeabb4996920810682b8
Paola P <dppuertol@gmail.com>
2025-12-03 12:41:12 +0000

    suppresion du return

--- 03-scope-variables/scope-variables.c
+++ 03-scope-variables/scope-variables.c
@@ -1,16 +1,15 @@
 /*work in progress*/
 #include <stdio.h>
 
-int main ()
+int main (void)
 
 {
     int a = 3; //variable dans le scope main
     printf("celle-ci est une variable avec un nombre entier dans le scope main: %d\n", a);
 
-return 0 ;
 
     { char b = 'p'; //variable dans le scope du bloc interne
-    printf("celle-ci est une variable avec un caractère : %c\n", b);
+    printf("celle-ci est une variable avec un caractère unique dans le scope du bloc supplémentaire: %c\n", b);
     return 0;
     }
 
