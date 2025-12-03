dae321417fec26fc03318f34259f412ba82ac522
Paola P <dppuertol@gmail.com>
2025-12-03 13:10:10 +0000

    test du scope de la variable b

--- 03-scope-variables/scope-variables.c
+++ 03-scope-variables/scope-variables.c
@@ -13,7 +13,14 @@ int main (void)
    ;
     }
 
-    /*    // Ici: b n'existe plus, car son scope est terminé.*/
+// erreur volontaire permettant de tester le scope*/
+
+//printf("celle-ce est une variable avec un caractère unique hors scope %c\n", b);
+//vs problem : 'b' undeclared (first use in this function) b n'existe plus ici
+//vs problem : identifier 'b' is undefined
+   
+/*Ici: b n'existe plus, car son scope est terminé.*/
+    
  return 0; // return à la fin de b permet de lire le bloc interne
 
 }
