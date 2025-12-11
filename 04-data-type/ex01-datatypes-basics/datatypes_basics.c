b23e6aaf372b94f1ea535a4f120eaad3383a0443
Paola P <dppuertol@gmail.com>
2025-12-11 12:18:12 +0000

    code déclaration de variables et affichage avec print

---
+++ 04-data-type/ex01-datatypes-basics/datatypes_basics.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (void)
+
+{
+//initialization
+long x = 100L;
+unsigned y = 17U;
+float b = 3.0F;
+
+//initial message
+printf("celle-ci est une valeur longue: %ld\n", x);
+printf("celle-ci est une valeur unsigned: %u\n", y);
+printf("celle-ci est une valeur float: %f\n", b );
+
+return 0;
+}
