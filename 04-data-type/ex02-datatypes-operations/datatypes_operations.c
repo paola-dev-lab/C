5be5faf7474277b28b5823a7ca537133f418e373
Paola P <dppuertol@gmail.com>
2025-12-13 15:08:41 +0000

    code: afficher la valeur de la dernière opération

--- 04-data-type/ex02-datatypes-operations/datatypes_operations.c
+++ 04-data-type/ex02-datatypes-operations/datatypes_operations.c
@@ -11,14 +11,17 @@ float c = 4.5F;
 /*Afficher les valeurs initiales*/
 printf("Celle-ci est une valeur longue %ld\n", a);
 printf("Celle-ci est une valeur unsigned %u\n", b);
-printf("celle-ci est une valeur float %f\n", c);
+printf("celle-ci est une valeur flottante %f\n", c);
 
 
 /*afficher les valeurs avec les operations*/
-long s = a + 1000l; // déclaration de la variable du type long s = a +1000
+long s = a + 1000l; // déclaration de la variable du type long qui stocke le resultat de: ajouter 1000 à 'a'
+unsigned m = b * 3u; // déclaration de la variable du type unsigned qui stocke le resultat de: b multiplié par 3
+float d = c / 2; //déclaration de la variable flottante du type flottante qui stocke le resultat de: c divisé par 2
+
 printf("%ld + 1000 = %ld\n", a, s);
-printf("Celle-ci est une valeur unsigned %u\n", b);
-printf("celle-ci est une valeur float %f\n", c);
+printf("%u multiplié par 3 = %u\n", b, m);
+printf("%f divisé par 2 = %f\n", c, d);
 
 
 return 0;
