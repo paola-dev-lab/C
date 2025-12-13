129d52eb3fa31b8513fbc37b6f2298c77c524605
Paola P <dppuertol@gmail.com>
2025-12-13 14:48:06 +0000

    code: afficher les valeurs initiales

---
+++ 04-data-type/ex02-datatypes-operations/datatypes_operations.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+
+{ 
+/*Déclaration de variables dans la fonction main*/
+long a = 5000L;
+unsigned b = 200U;
+float c = 4.5F;
+
+/*Afficher les valeurs initiales*/
+printf("Celle-ci est une valeur longue %ld\n", a);
+printf("Celle-ci est une valeur unsigned %u\n", b);
+printf("celle-ci est une valeur float %f\n", c);
+
+
+
+
+return 0;
+}
+
