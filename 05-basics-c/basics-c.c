693f3b3907979cb10be99ad9d0f6e5523be5c9eb
paola <dppuertol@gmail.com>
2026-01-05 21:27:01 +0000

    code:declaration de variables et affichage

--- 05-basics-c/basics-c.c
+++ 05-basics-c/basics-c.c
@@ -3,9 +3,15 @@
 int main(void)
 
 {
-	printf("Hello piscine 42");
+	int a = 10; // déclaration de la variable a
+	int b = 3; // déclatration de la variable b
+	
+       printf("a= %d, b= %d\n", a, b);
+
+       printf("Hello piscine 42\n");
+
 	
 
-return 0;
+return (0);
 
 }
