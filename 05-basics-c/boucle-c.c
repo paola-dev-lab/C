a0c6f55835a057c89e100e5782ff536b58b56e45
paola <dppuertol@gmail.com>
2026-01-06 09:15:30 +0000

    code boucle simple

---
+++ 05-basics-c/boucle-c.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+
+{
+	int i = 0; // déclaration de la variable i
+      
+	
+	while (i<5) //condition de la boucle
+        {
+	printf("i = %d\n", i);
+	i++;
+
+	}
+
+	return (0);
+
+}
