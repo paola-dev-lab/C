1ee814f273de16135cee0a50ee6e1c96e6c3bccc
paola <dppuertol@gmail.com>
2026-01-07 09:56:24 +0000

    ajout des commentaires

--- 07-tableaux.c/tableau01.c
+++ 07-tableaux.c/tableau01.c
@@ -7,20 +7,21 @@ int main(void) // point d'entrée du code
  int i; // déclaration de la variable i (indice)
  int tab [5]; //déclaration du tableau avec des noçmbres entiers et 5 valeurs
 
- /* */ 
+ /*initialization du tableau avec les 5 valeurs case par case */
+ /* une autre option d'initializer les valeurs du tableau : int tab[5] = {10, 20, 30, 40, 50};*/
  tab[0] = 10; 
  tab[1] = 20;
  tab[2] = 30;
  tab[3] = 40;
  tab[4] = 50;
 
-for(i=0; i<5; i++)
+for(i=0; i<5; i++)//boucle for : commence par l'indice 0, autant que i soit inférieur à 5, incremente d'une unité 
 
 {
-	printf("tab[%d] = %d\n", i, tab[i]);
+	printf("tab[%d] = %d\n", i, tab[i]); // si la condition de la boucle est respecté affiche x
 
 }
 
 
-return (0);
+return (0); // sortie avec code executée correctement
 }
