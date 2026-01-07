13fab69ad58a28f3f53bc3ce8c940d346f490d72
paola <dppuertol@gmail.com>
2026-01-07 08:26:14 +0000

    ajout des commentaires

--- 06-boucles/boucle_for.c
+++ 06-boucles/boucle_for.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <stdio.h> // preprocessor : appel au dossier stdio.h pour processer ce code
 
-int main(void)
-{int i;
+int main(void) // fonction principale
+{int i; //déclaration de lavariable i
 
-for (i=1; i<=5; i++)
+for (i=1; i<=5; i++) // boucle for : initialization; condition; incremetation
 
-{ printf("i=%d\n", i);
+{ printf("i=%d\n", i); //instruction
 }
-return (0);
+return (0); //code d'execution correcte
 }
