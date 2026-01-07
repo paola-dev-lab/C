83a51536e97d4f4d1178903627c3a733bc0c3de0
paola <dppuertol@gmail.com>
2026-01-07 14:51:49 +0000

    code: ajout du compteur de chiffres paires affichées

--- 07-tableaux.c/tableau02.c
+++ 07-tableaux.c/tableau02.c
@@ -4,17 +4,23 @@ int main(void)
 
 
 {
- int i; //declaration de la variable i (indice)
- int tableau[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100}; //declaration du tableau et initialization des valeurs
-
-for(i=0;i<10;i++) //parcourir le tableau
-{
-printf("voici une valeur pair [%d]\n", tableau[i]); //affichage des valeur
-}
+ int i = 0; //declaration de la variable i (indice) et initialization de i(compteur) à 0
+ int tableau[10] = {10, 21, 30, 41, 50, 60, 71, 80, 91, 100}; //declaration du tableau et initialization des valeurs
 
 int size = sizeof(tableau)/ sizeof(tableau[0]); // calculer la taille du tableau
 
 printf("voici nombre de valeurs du tableau: %d\n", size); // afficher le calcul da la taille du tableau
+
+/*Trouver les nombres pairs dans le tableau */
+while (i < size) // implemente l'instruction tant que l'indice est inférieur à la taille du tableau 
+{
+
+	if (tableau[i] % 2 == 0) 
+	{printf("nombres pairs : %d\n", tableau[i]);}
+
+i++; //incremente le compteur 
+
+}
 
 return (0);
 }
