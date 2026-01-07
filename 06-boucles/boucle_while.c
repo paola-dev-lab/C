6eb49ede837b74898746628470d86f9f005eb6a1
paola <dppuertol@gmail.com>
2026-01-07 07:41:06 +0000

    changement de nom

---
+++ 06-boucles/boucle_while.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+int i = 42; // déclaration et initialization de la varaible
+
+while (i>=0) // condition de la boucle
+	    
+   {printf("voici la variable en boucle %d\n", i);//instruction 1
+   i = i - 1; // instruction 2: mise à jour de la variable i
+   }
+
+return (0); //code executé correctement
+
+
+}
