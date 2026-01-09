392a1f99e489fd914ed01b9034485bd2965919cb
paola <dppuertol@gmail.com>
2026-01-09 08:18:10 +0000

    ajout de commentaires

--- 08-chaîne_de_caractères/char01.c
+++ 08-chaîne_de_caractères/char01.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
+#include <stdio.h> //appel à la bibliothèque : pre processor"
 
-int main(void)
+int main(void) // déclaration de la fonction principale
 
 {
-char str[] = "Hello";
-int i = 0;
+char str[] = "Hello"; // déclaration et intitialization de la chaîne de caractères
+int i = 0; // déclaration de l'indice
 
-while (str[i] != '\0')
+while (str[i] != '\0') //condition
 
-   {
+   { //bloc d'instructions 
 	   printf("%c\n", str[i]);
 	   i++;
    }
-            printf("longueur = %d\n",i);
+            printf("longueur = %d\n",i);//afficher la longueur en dehors de la boucle
 
 
-return (0);
+return (0); //execution correcte
 }
