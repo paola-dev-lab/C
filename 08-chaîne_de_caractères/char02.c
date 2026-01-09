ff71e846303b4498ab04da1dc4c70faeb1a31210
paola <dppuertol@gmail.com>
2026-01-09 20:40:32 +0000

    ajout de  commentaires

--- 08-chaîne_de_caractères/char02.c
+++ 08-chaîne_de_caractères/char02.c
@@ -10,23 +10,23 @@ int main(void)
 	int count_spaces = 0; //déclaration du compteur de spaces 
 
 
-while (str[i] != '\0') //parcourir la chaîne
+while (str[i] != '\0') //parcourir la chaîne jusqu'à la	valeur nulle
 
  { printf("%c\n", str[i]); //afficher catactère par caractère
 	      
-       if (str[i] == ' ')
+       if (str[i] == ' ') // première condition : detection de vides
 
-		       count_spaces++;
+		       count_spaces++; // augment le compteur d'espaces de 1
 
-	 else if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
-	    count_letters++;
+	 else if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z')) //deuxième condition detection de mayuscules et minuscules
+	    count_letters++; // incrementation du compteur de lettres
 
-	 i++;
+	 i++; //augmente l'indice de 1
        
  }
  
-printf("nombre total d'espaces : %d\n", count_spaces);
-printf("nombre total de lettres : %d\n", count_letters);
+printf("nombre total d'espaces : %d\n", count_spaces); //affichage de la valeur du compteur d'espaces
+printf("nombre total de lettres : %d\n", count_letters); //affichage de la valeur du compteur de lettres
 
 return (0); 
 
