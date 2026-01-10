a1730d5d2c3621d2872a10afa1c8e99ef43a3911
paola <dppuertol@gmail.com>
2026-01-10 10:29:42 +0000

    ajoute de commentaires

--- 08-chaîne_de_caractères/02-strings_fonctions/ex01.c
+++ 08-chaîne_de_caractères/02-strings_fonctions/ex01.c
@@ -1,36 +1,37 @@
 #include <stdio.h>
 
 
-int count_letters(char str[])
+int count_letters(char str[]) // fonction count letters
 
 {
 
-	int i = 0;
-	int count = 0;
+	int i = 0; //déclaration et initialization de l'indice
+	int count = 0; //déclaration et initialization du compteur
 
       		
-	while (str[i] != '\0')	
+	while (str[i] != '\0') //condition d'iteration : parcour le tableau
                 {
-			if ((str[i] >= 'a' && str[i] <= 'z')
-		        || (str[i] >= 'A' && str[i] <= 'Z'));
+			if ((str[i] >= 'a' && str[i] <= 'z') //condition si lettre minuscule
+		        || (str[i] >= 'A' && str[i] <= 'Z')); //condition: si lettre mayuscule
 			{
-				count++;
+				count++; // alors : increment count de 1
 			}
-		i++;
+		i++; // increment l'indice de 1
 		}
-      return (count);	
+      return (count); //Si executé correctement : sortie positive	
 }
 
-int main(void)
+int main(void) //fonction principale
 
 {
 
   
-char str[] = "Hola mundo cruel";
-int result;
+char str[] = "Hola mundo cruel"; //declaration et initalization d'une chaîne
+int result; //déclaration de la variable result
 
-result = count_letters(str);
-printf("nombre de lettres : %d\n", result);
+result = count_letters(str); //result est l'application de la fonction count_letters à la chaîne initialisée
+			     //
+printf("nombre de lettres : %d\n", result); //appel à la fonction couent_letter par le biais de la variable result.
 
 return (0);
 }
