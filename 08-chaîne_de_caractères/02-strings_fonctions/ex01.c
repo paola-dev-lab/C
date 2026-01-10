68f6d9011ef945233adf226d511d5a8ace5dfa09
paola <dppuertol@gmail.com>
2026-01-10 10:16:40 +0000

    code: fonction count

---
+++ 08-chaîne_de_caractères/02-strings_fonctions/ex01.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+int count_letters(char str[])
+
+{
+
+	int i = 0;
+	int count = 0;
+
+      		
+	while (str[i] != '\0')	
+                {
+			if ((str[i] >= 'a' && str[i] <= 'z')
+		        || (str[i] >= 'A' && str[i] <= 'Z'));
+			{
+				count++;
+			}
+		i++;
+		}
+      return (count);	
+}
+
+int main(void)
+
+{
+        
+
+
+
+char str[];
+
+
+
+
+}
