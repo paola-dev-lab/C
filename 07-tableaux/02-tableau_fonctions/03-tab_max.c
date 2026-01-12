661d4d84a3f70c37cd933965f656ec656a3b6066
paola <dppuertol@gmail.com>
2026-01-12 10:59:11 +0000

    ajout des commentaires

--- 07-tableaux/02-tableau_fonctions/03-tab_max.c
+++ 07-tableaux/02-tableau_fonctions/03-tab_max.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 
-int max_array(int arr[], int size)
+int max_array(int arr[], int size) // déclaration de la fonction
 
  {
+	 /* déclaration des variables*/ 
     int i = 0;
-    int num_max = arr[0];
+    int num_max = arr[0];  
 
-    while  (i < size) 
+    while  (i < size)  //parcour le tableau et initialization de la boucle
     {
-      if (arr[i] > num_max)
+      if (arr[i] > num_max) // condition : compare la valeur du tableau avec la valaue stockée dans num_max
      
-      	      num_max = arr[i];  
+      	      num_max = arr[i];  // met à jour la valeur de num_max si la condition est respectée
 
-      i++;
+      i++; // augment l'indice de 1
     }
 
-    return (num_max);
+    return (num_max); // execution correcte
  }
 
-int main(void)
-{
-	int arr[] = { 12, 14, 3, 15, 100, 500 };
-       int size = sizeof(arr) / sizeof(arr[0]);
-        int result = max_array(arr, size);
+int main(void)  // fonction main
 
-           printf("max : %d\n", result);
+{/* déclaration des variables*/
+	int arr[] = { 12, 14, 3, 15, 100, 500 }; // tableau 
 
-         return (0);
+       int size = sizeof(arr) / sizeof(arr[0]); //taille du tableau
+        int result = max_array(arr, size); //appel à la function max_array avec les variables
+
+           printf("max : %d\n", result); //affichage
+
+         return (0); //execution correcte
 }
