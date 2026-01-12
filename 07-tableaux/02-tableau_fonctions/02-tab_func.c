e468f87e6479df712be01a3787403f9011d4f453
paola <dppuertol@gmail.com>
2026-01-12 11:11:16 +0000

    ajout des commentaires

--- 07-tableaux/02-tableau_fonctions/02-tab_func.c
+++ 07-tableaux/02-tableau_fonctions/02-tab_func.c
@@ -1,36 +1,38 @@
 #include <stdio.h>
 
-int count_even(int arr[], int size)
+int count_even(int arr[], int size) //déclaration de la fonction
 
 {
-   int i = 0;
+
+	/*déclaration des variables */
+   int i = 0; 
    int count = 0; 
 
-   while (i < size)
+   while (i < size) //initialization de la boucle
    {
-      if (arr[i] % 2 == 0)
+      if (arr[i] % 2 == 0) // condition 1
         
-	      count_even++;
+	      count_even++; //action a executer si condition respectée
        
-   i++;
+   i++;//incrementation de l'indice
    } 
 
-   return (count_even);
+   return (count_even);//zxzcution correcte
 
 }
 
-int main(void)
+int main(void)  // fonction main
 
-{
+{ /* déclaration desz varibles*/
 
-  int arr[] = {12, 13, 14, 15, 16, 17, 18};
-  int size = sizeof(arr) / sizeof(arr[0]);
+  int arr[] = {12, 13, 14, 15, 16, 17, 18}; //tableau
+  int size = sizeof(arr) / sizeof(arr[0]); //taille du tableau
 
-  int result = count_even(arr, size);
+  int result = count_even(arr, size); // appel à la fonction avec ses variables
 
-    printf("voici les nombres pairs : %d\n", result);
+    printf("voici les nombres pairs : %d\n", result); // affichage
 
-  return (0);  
+  return (0);  //execution OK
 
 }
 
