8bc753f38f1efa70c2142c633a6b0eb519892fcc
paola <dppuertol@gmail.com>
2026-01-13 10:18:18 +0000

    code: fonction corrigé + appel au code dans main

--- 07-tableaux/02-tableau_fonctions/06-touch_average.c
+++ 07-tableaux/02-tableau_fonctions/06-touch_average.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
 
 
-int average_array(int int_arr[], int size)
+int average_array(int arr[], int size)
 {
 
   int i = 1;//initialiser le i et le sum
   int sum = arr[0];
-  int aver;
+  int aver = 0;
 
    //parcourir le tableau avec une boucle
   while ( i < size)
    {
       sum += arr[i];  //Adittioner chaque valeur
+      i++;
    }
 
   aver = sum / size; //calculer la moyenne après la boucle
 
 //retourner la moyenne
  return (aver); 
+}
+
+int main(void)
+
+{ int arr[] = { 10, 20, 30, 40};
+  int size = sizeof(arr) / sizeof(arr[0]);
+
+  int result = average_array(arr, size); 
+
+  printf("moyenne : %d\n", result);
+
+  return (0);
 }
 
 
