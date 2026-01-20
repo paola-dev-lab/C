164ca621cc38da272300fea52a658f551d5b6cb9
paola <dppuertol@gmail.com>
2026-01-20 08:43:51 +0000

    ajout de commentaires explicatifs

--- 09-pointeurs/04-pointeurs_chaines/01-longueur_chaine.c
+++ 09-pointeurs/04-pointeurs_chaines/01-longueur_chaine.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 
-int ft_strlen(char *str)
+int ft_strlen(char *str) //déclaration de la fonction avec un pointeur vers une chaîne de caractères comme paramètre
 
 {
   int i = 0;
 
 
 
-  while (str[i] != '\0')
+  while (str[i] != '\0') //parcourt le tableau jusu'à trouver la valeur nulle
   { 
  
-    i++;  
+    i++;  //augmente l'indice
   }
 
-return (i);
+return (i); //renvoie le nimbre de caractères
 }
 
 int main(void)
 
 {
-   char s[] = "Parisien 42"; 
+   char s[] = "Parisien 42"; //déclaration d'une chaîne de caractères
  
    
-  int len = ft_strlen(s);  
+  int len = ft_strlen(s); //stockage de la fonction ft_strlen dans len ayant comme paramètre s
 
-  printf("nombre de caractères : %d\n", len);
+  printf("nombre de caractères : %d\n", len); //affichage
 
   return (0);
 
