450acadf7376c5dece11c63301d0e71b63d47dab
paola <dppuertol@gmail.com>
2026-01-22 06:59:13 +0000

    ajout des commentaires explicatifs

--- 09-pointeurs/07-strings_and_write/01-ft_putchar.c
+++ 09-pointeurs/07-strings_and_write/01-ft_putchar.c
@@ -1,9 +1,17 @@
+/*la fonction affiche un caractèr en lisan sa valeur à partir de la mémoire */
+
 #include <unistd.h>
 
-void ft_putchar(char c)
+void ft_putchar(char c) 
+	
+//void ne calcule rien et en renvoie aucun valeur; elle effectue un affichage. 
 
 {
- write ( 1, &c, 1);
+
+/*write lit les octet en mémoire à partir de l'adresse donnée et le écrit sur la sortie standard selon la taille indiquée */
+
+   write ( 1, &c, 1); 
+//&c est l'adresse de c, write reçoit une adresse en paramètre
   
 }
 
