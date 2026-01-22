7becc39318b3a8993aafd8ff36d1e57a6bf47187
paola <dppuertol@gmail.com>
2026-01-22 07:45:59 +0000

    ajout des commentaires explicatifs

--- 09-pointeurs/07-strings_and_write/02-ft_putstr.c
+++ 09-pointeurs/07-strings_and_write/02-ft_putstr.c
@@ -1,3 +1,5 @@
+/*La fonction affiche une chaîne de caractères stockée en mémoire à partir de l'adresse du premier caractère*/ 
+
 #include <unistd.h>
 
 void ft_putstr(char *str)
@@ -5,15 +7,17 @@ void ft_putstr(char *str)
 {
  int i = 0;
 
- while (str[i] != '\0')
+ while (str[i] != '\0') //parcourt la chaîne à partir de l'adresse du premier caractère 
    {
       { 
-	write (1, &str[i], 1);
+	write (1, &str[i], 1); //
       }
  i++;
    }
 
 }
+
+  /*test de la fonction*/
 
 int main (void)
 {
